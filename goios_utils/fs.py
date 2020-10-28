6b22a2ad953207be6e89c75ff287647f6c82e96a
Andre Goios <a.almeida@unsw.edu.au>
2020-10-28 15:56:55 +1100

    nist file reader

--- goios_utils/fs.py
+++ goios_utils/fs.py
@@ -24,6 +24,31 @@ def find_ext_volume(vol_name):
         if os.name == 'nt':
             if get_label_w(mp).find(vol_name) > -1:
                 return mp
+            else:
+                raise FileNotFoundError
         else:
             if mp.find(vol_name) > -1:
                 return mp
+            else:
+                raise FileNotFoundError
+
+
+def nist_read(filename):
+    import subprocess
+    import io
+    from scipy.io import wavfile
+
+    proc = subprocess.run(['sph2pipe','-f', 'wav', filename],stdout=subprocess.PIPE)
+    riff_chunk_size = len(proc.stdout) - 8
+    # Break up the chunk size into four bytes, held in b.
+    q = riff_chunk_size
+    b = []
+    for i in range(4):
+        q, r = divmod(q, 256)
+        b.append(r)
+
+    # Replace bytes 4:8 in proc.stdout with the actual size of the RIFF chunk.
+    riff = proc.stdout[:4] + bytes(b) + proc.stdout[8:]
+
+    #riff=proc.stdout
+    sr, w = wavfile.read(io.BytesIO(riff))
